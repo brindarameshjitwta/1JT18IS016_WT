aeb33094a00a02e95e273c6681a85572ba3c3b33
brindarameshjitwta <82956588+brindarameshjitwta@users.noreply.github.com>
2021-07-02 21:02:34 +0530

    Add files via upload

--- Program 6/Program6.php
+++ Program 6/Program6.php
@@ -1,1 +1,33 @@
+<?php
+$user = "YourName";
+?>
+<!DOCTYPE html>
+<html>
+<head> <title> program 6 </title>
+<style>
+	body
+	{
+		background-color: blue;
+		color: white
+	}
+  
+</style>
 
+</head>
+<body>
+<h2><center><?php echo $user; ?>,Welcome to Sample PHP Script</center> </h1>
+<?php
+print "<h2>This page displays the number of page visits </h2>";
+print "<h3> REFRESH PAGE </h3>";
+$name="counter.txt";
+$file = fopen($name,"r");
+$hits = fscanf($file,"%d");
+fclose($file);
+$hits[0]++;
+$file = fopen($name,"w");
+fprintf($file,"%d",$hits[0]);
+fclose($file);
+print "Total number of views: ".$hits[0];
+?>
+</body>
+</html>
