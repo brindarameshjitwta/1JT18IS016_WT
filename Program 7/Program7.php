b6103b40ebda6bc3dd53354ed19f4561de168c3a
brindarameshjitwta <82956588+brindarameshjitwta@users.noreply.github.com>
2021-07-02 21:06:22 +0530

    Add files via upload

--- Program 7/Program7.php
+++ Program 7/Program7.php
@@ -1,1 +1,35 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="refresh" content="1" />
+<style>
+	p
+	{
 
+    font-size: 39px;
+    position: absolute;
+    top: 38%;
+    left: 35%;
+    display: inline-block;
+             
+	}
+
+body
+{
+	background-color: blue;
+
+
+}
+</style>
+ <p>
+<?php 
+echo "The server time is".date(" h: i : s A"). "<br>";
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+
+?> 
+</p>
+</head>
+</html>
